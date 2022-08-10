9e95bfdf9c9e8b8c4999fee1072fcf7d7bf83ca8
YasabnehT <yastesh10@gmail.com>
2022-08-10 23:21:48 +0300

    topic modeling and sentiment analyis done

--- clean_tweets_dataframe.py
+++ clean_tweets_dataframe.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 class Clean_Tweets:
     """
     The PEP8 Standard AMAZING!!!
@@ -21,15 +22,25 @@ class Clean_Tweets:
         """
         drop duplicate rows
         """
+        # identify duplicates with id
         df.drop_duplicates(subset = 'id', keep=False,inplace=True)
         df = df[df['id'] != 'id']
         return df
-    def convert_to_datetime(self, df:pd.DataFrame)->pd.DataFrame:
+    def convert_to_datetime(self, df:pd.DataFrame,tweets)->pd.DataFrame:
         """
         convert column to datetime
         """
-        df[df['created_at']] = pd.to_datetime(df[df['created_at']],format="%Y-%m-%d")
-        df = df[df['created_at'] >= '2022-07-31' ]
+        df = pd.DataFrame(data =[tweet.text for tweet in tweets], columns="tweets")
+        df["id"] = np.array([tweet.id]for tweet in tweets)
+        df['text'] = np.array([tweet.text for tweet in tweets])
+        df['text_leng'] = np.array([len(tweet.text) for tweet in tweets])
+        df['date'] = np.array([tweet.created_at for tweet in tweets])
+        df['source'] = np.array([tweet.source for tweet in tweets])
+        df['likes'] = np.array([tweet.favorite_count for tweet in tweets])
+        df['retweets'] = np.array([tweet.retweet_count for tweet in tweets])
+        
+        # df[df['created_at']] = pd.to_datetime(df[df['created_at']],format="%Y-%m-%d")
+        # df = df[df['created_at'] >= '2022-07-31' ]
         return df
     
     def convert_to_numbers(self, df:pd.DataFrame)->pd.DataFrame:
---
+++ sentiment_analysis.py
@@ -0,0 +1,8 @@
+#%%
+import numpy as np
+import pandas as pd 
+import re
+import nltk 
+import matplotlib.pyplot as plt
+import seaborn as sns
+%matplotlib inline
